e122f619852f5dddf42257c9e771c4f491e362ff
tsolawoyin <t.olawoyin@outlook.com>
2025-01-06 05:39:48 +0100

    Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import services from "./services/veraAI";
-
+import logo from "./assets/revv-10.jpg"
 import "./App.css";
 
 // that's it...
@@ -9,8 +9,8 @@ import "./App.css";
 // nice and easy...
 const greetingsArr = [
   "Hi, I'm VeraAI. Enter any number in words and I will magically turn it into digits.",
-  "Hi. I'm VeraAI. I'm here to help you convert words to digits"
-]
+  "Hi. I'm VeraAI. I'm here to help you convert words to digits",
+];
 
 function App() {
   // now let's try to get data from backend
@@ -19,6 +19,7 @@ function App() {
   let [posn, setPosn] = useState(0);
   let [inputVal, setInputVal] = useState("");
   let [loading, setLoading] = useState(false);
+  let inputEl = useRef(null);
 
   // This is just for simple animation....
   let greetingTimeout = setTimeout(() => {
@@ -40,35 +41,39 @@ function App() {
     setLoading(true);
 
     services
-    .computeVal({
-      sentence: inputVal,
-    })
-    .then((e) => {
-      // console.log(e); // makes sense?
-      let val = e.val;
-      if (val) {
-        setBaseString(`${inputVal} in digits is ${val}`);
-        setGreeting("");
-        setPosn(0);
-        setInputVal("");
-        setLoading(false);
-      } else {
-        setBaseString(`Sorry, I can't understand your statement`);
-        setGreeting("");
-        setPosn(0);
-        setInputVal("");
-        setLoading(false);
-      }
-    });
+      .computeVal({
+        sentence: inputVal,
+      })
+      .then((e) => {
+        // console.log(e); // makes sense?
+        let val = e.val;
+        if (val) {
+          setBaseString(`${inputVal} in digits is ${val}`);
+          setGreeting("");
+          setPosn(0);
+          setInputVal("");
+          setLoading(false);
+        } else {
+          setBaseString(`Sorry, I can't understand your statement`);
+          setGreeting("");
+          setPosn(0);
+          setInputVal("");
+          setLoading(false);
+        }
+      });
   };
 
   useEffect(() => {
-    setBaseString(greetingsArr[Math.floor(Math.random() * greetingsArr.length)])
+    setBaseString(
+      greetingsArr[Math.floor(Math.random() * greetingsArr.length)]
+    );
+    inputEl.current.focus();
   }, []);
 
   return (
     <main>
-      <div>
+      <div className="chat">
+        <img src={logo} alt="v-img" />
         <h1>{greetings}</h1>
         <form onSubmit={handleSubmit}>
           <input
@@ -77,9 +82,26 @@ function App() {
             placeholder="one hundred and five"
             value={inputVal}
             onChange={handleChange}
+            ref={inputEl}
           />
-          <button className={`button is-warning ${loading ? "is-loading" : ""}`}>Convert</button>
+          <button
+            className={`button is-warning ${loading ? "is-loading" : ""}`}
+          >
+            Convert
+          </button>
         </form>
+        <div className="footerx">
+          <p>A very basic full-stack web application</p>
+          <div className="code-info">
+            <p>tsolawoyin</p>
+            <p>|</p>
+            <p>Apera Veronica</p>
+            <p>|</p>
+            <p>
+              <a href="https://github.com/tsolawoyin/VeraAI">source</a>
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
